6f3566cf9be4e96942c1f38df7a3cfc5f3e10f74
GTUNE-001\markz <undoworks4649@gmail.com>
2025-05-16 22:52:24 +0900

    Initial commit

---
+++ test/auto_crypto-hono.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AutoCryptoHono from '../lib/auto_crypto-hono-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/auto_crypto-hono-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AutoCryptoHono.AutoCryptoHonoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
